a8b8c0126f874ba09cb5c34946bbcbaa03a4de32
Jude Payne <judepayne@yahoo.co.uk>
2024-03-17 12:04:27 +0000

    git rm moved files

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-containerprog=${1:-docker}
-
-portfile="resources/PORT"
-
-port=$(cat "$portfile")
-
-$containerprog run --env-file docker/tala.env -dp 127.0.0.1:$port:$port dictim-server
